f22b20a482d3f3489a2100b22201cdc99d147256
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-10-25 23:26:12 -0400

    wip

--- src/card-counter.js
+++ src/card-counter.js
@@ -1,1 +1,18 @@
-const { program } = require("commander");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .name("card-counter.js")
+  .description(
+    "CLI, calculate how many receiving cards are needed for a given screen size."
+  )
+  .version("0.0.1");
+
+program
+  .command("getRes")
+  .description("Get the maximum resolution of a specified card")
+  .argument("<string>", "enter the model number of the card")
+  .action(() => {
+    // Do work here
+    console.log("resolution: ");
+  });
